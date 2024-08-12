4434b586c90c90b5acd39e3f96f8532d38a93e8f
Lukas Rosenfeldt <lukasrosenfeldt@gmail.com>
2024-08-12 10:58:26 +0200

    feat(api-clients): add client for payment execution, payment information and order management

--- src/endpoints/index.ts
+++ src/endpoints/index.ts
@@ -1,2 +1,5 @@
 export { CheckoutApiClient } from './CheckoutApiClient.js';
 export { CommerceCaseApiClient } from './CommerceCaseApiClient.js';
+export { OrderManagementCheckoutActionsApiClient } from './OrderManagementCheckoutActionsApiClient.js';
+export { PaymentExecutionApiClient } from './PaymentExecutionApiClient.js';
+export { PaymentInformationApiClient } from './PaymentInformationApiClient.js';
