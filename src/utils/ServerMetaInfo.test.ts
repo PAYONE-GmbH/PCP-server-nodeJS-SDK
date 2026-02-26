9bdbc1b5e9cfd22c218aa1b1dccb27083ff24931
Dirk Ehrhardt <d.ehrhardt@nanogiants.de>
2026-02-26 09:29:27 +0100

    chore: update SDK version to 1.6.0 in package.json and package-lock.json, and adjust sdkIdentifier

--- src/utils/ServerMetaInfo.test.ts
+++ src/utils/ServerMetaInfo.test.ts
@@ -13,7 +13,7 @@ describe('ServerMetaInfo', () => {
     expect(serverMetaInfo.platformIdentifier).toBe(
       `${os.platform()}, node version is: ${process.version}`,
     );
-    expect(serverMetaInfo.sdkIdentifier).toBe('NodeServerSDK/v1.5.1');
+    expect(serverMetaInfo.sdkIdentifier).toBe('NodeServerSDK/v1.6.0');
     expect(serverMetaInfo.sdkCreator).toBe('PAYONE GmbH');
   });
 });
