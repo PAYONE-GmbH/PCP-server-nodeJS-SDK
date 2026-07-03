29ec02033702786388503fd5ed82ea17092f8dfd
Dirk Ehrhardt <d.ehrhardt@nanogiants.de>
2026-07-03 06:34:37 +0200

    feat: update API version to 1.57.0

--- src/models/CancelPaymentResponse.ts
+++ src/models/CancelPaymentResponse.ts
@@ -1,5 +1,7 @@
+import type { FundSplit } from './FundSplit.js';
 import type { PaymentResponse } from './PaymentResponse.js';
 
 export interface CancelPaymentResponse {
   payment?: PaymentResponse;
+  fundSplit?: FundSplit;
 }
